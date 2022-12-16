5149b2d1650a14aec0d1b7158917ab9dccd1d714
Gabriela Rosa <88403395+Gabi-Rosa@users.noreply.github.com>
2022-12-16 17:30:22 -0300

    Second commit
    alteração na pagina footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,24 +5,24 @@ import {Link} from 'react-router-dom'
 const Footer = () =>{
     return(
         <div>   
-             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p class="col-md-4 mb-0 text-muted"> &copy; Direitos Reservados</p>
+             <footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+                <p class="col-md-5 mb-1  text-muted py-2"> &copy; Direitos Reservados</p>
                 
-                <ul class="nav col-md-4 justify-content-end">
-                    <li className='nav-item'>Siga-nos nas Redes Socias
-                            <li className='nav-item'>
+                <ul class="nav  col-md-4 justify-content-center">
+                    <li className='nav-item py-2'>Siga-nos nas Redes Socias</li>
+                            <li className='nav-item col-md-2 px-2'>
                                 <Link className='nav-link px-2 text-muted'> <i className='bi bi-instagram'></i>
                                 </Link>
                             </li>
 
-                            <li className='nav-item'> 
+                            <li className='nav-item col-md-2'> 
                                 <Link className='nav-link px-2 text-muted'><i className='bi bi-envelope '></i></Link>
                             </li>
 
-                            <li className='nav-item'>
+                            <li className='nav-item col-md-2 py-2'>
                                 <Link><i className='bi bi-facebook'></i></Link>
                             </li>
-                    </li>
+                    
                 </ul>
             </footer>
         </div>
